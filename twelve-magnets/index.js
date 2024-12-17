fe8ed2c379f7a0b562be5c260a8af4983819db4d
Andrew Lehman <andrew.lehman364@gmail.com>
2024-12-17 00:00:34 -0600

    parameters on twelve magnets

--- twelve-magnets/index.js
+++ twelve-magnets/index.js
@@ -1,108 +1,107 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d', { willReadFrequently: true });
 
-// TODO: parameterize all the things
-canvas.width = 640;
-canvas.height = 360;
+let shouldDrawMags = false;
+let shouldDrawBalls = true;
+let shouldDither = true;
 
-const gravity = 0;
-const friction = 0.01;
-const bounceFactor = 0.9;
-const terminalV = 2;
-const magConstant = 2;
+let gravity = 0;
+let friction = 0.01;
+let bounceFactor = 0.9;
+let terminalV = 2;
+let magConstant = 2;
+let magRadius = 10;
+let ballRadius = 10;
+let ballOpacity = 0.1;
+let backgroundColor = 10;
+let ballFeatheringSteps = 5;
+let ballFeatheringThickness = 1;
 
-const twelveNotes = [ "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" ];
+let ballCount = 100;
+let canvasWidth = 320;
+let canvasHeight = 180;
 
-const notes = {
-    "A0": 27.50,
-    "A#0": 29.14,
-    "B0": 30.87,
-    "C1": 32.70,
-    "C#1": 34.65,
-    "D1": 36.71,
-    "D#1": 38.89,
-    "E1": 41.20,
-    "F1": 43.65,
-    "F#1": 46.25,
-    "G1": 49.00,
-    "G#1": 51.91,
-    "A1": 55.00,
-    "A#1": 58.27,
-    "B1": 61.74,
-    "C2": 65.41,
-    "C#2": 69.30,
-    "D2": 73.42,
-    "D#2": 77.78,
-    "E2": 82.41,
-    "F2": 87.31,
-    "F#2": 92.50,
-    "G2": 98.00,
-    "G#2": 103.83,
-    "A2": 110.00,
-    "A#2": 116.54,
-    "B2": 123.47,
-    "C3": 130.81,
-    "C#3": 138.59,
-    "D3": 146.83,
-    "D#3": 155.56,
-    "E3": 164.81,
-    "F3": 174.61,
-    "F#3": 185.00,
-    "G3": 196.00,
-    "G#3": 207.65,
-    "A3": 220.00,
-    "A#3": 233.08,
-    "B3": 246.94,
-    "C4": 261.63,
-    "C#4": 277.18,
-    "D4": 293.66,
-    "D#4": 311.13,
-    "E4": 329.63,
-    "F4": 349.23,
-    "F#4": 369.99,
-    "G4": 392.00,
-    "G#4": 415.30,
-    "A4": 440.00,
-    "A#4": 466.16,
-    "B4": 493.88,
-    "C5": 523.25,
-    "C#5": 554.37,
-    "D5": 587.33,
-    "D#5": 622.25,
-    "E5": 659.26,
-    "F5": 698.46,
-    "F#5": 739.99,
-    "G5": 783.99,
-    "G#5": 830.61,
-    "A5": 880.00,
-    "A#5": 932.33,
-    "B5": 987.77,
-    "C6": 1046.50,
-    "C#6": 1108.73,
-    "D6": 1174.66,
-    "D#6": 1244.51,
-    "E6": 1318.51,
-    "F6": 1396.91,
-    "F#6": 1479.98,
-    "G6": 1567.98,
-    "G#6": 1661.22,
-    "A6": 1760.00,
-    "A#6": 1864.66,
-    "B6": 1975.53,
-    "C7": 2093.00,
-    "C#7": 2217.46,
-    "D7": 2349.32,
-    "D#7": 2489.02,
-    "E7": 2637.02,
-    "F7": 2793.83,
-    "F#7": 2959.96,
-    "G7": 3135.96,
-    "G#7": 3322.44,
-    "A7": 3520.00,
-    "A#7": 3729.31,
-    "B7": 3951.07,
-    "C8": 4186.01,
-};
+document.querySelector("#mag-radius").oninput = function () {
+    magRadius = parseFloat(this.value);
+    document.querySelector("#mag-radius-value").innerHTML = magRadius;
+}
+
+document.querySelector("#ball-radius").oninput = function () {
+    ballRadius = parseFloat(this.value);
+    document.querySelector("#ball-radius-value").innerHTML = ballRadius;
+}
+
+document.querySelector("#gravity").oninput = function () {
+    gravity = parseFloat(this.value);
+    document.querySelector("#gravity-value").innerHTML = gravity;
+}
+
+document.querySelector("#friction").oninput = function () {
+    friction = parseFloat(this.value);
+    document.querySelector("#friction-value").innerHTML = friction;
+}
+
+document.querySelector("#bounce-factor").oninput = function () {
+    bounceFactor = parseFloat(this.value);
+    document.querySelector("#bounce-value").innerHTML = bounceFactor;
+}
+
+document.querySelector("#terminal-velocity").oninput = function () {
+    terminalV = parseFloat(this.value);
+    document.querySelector("#terminal-velocity-value").innerHTML = terminalV;
+}
+
+document.querySelector("#mag-constant").oninput = function () {
+    magConstant = parseFloat(this.value);
+    document.querySelector("#mag-constant-value").innerHTML = magConstant;
+}
+
+document.querySelector("#background-color").oninput = function () {
+    backgroundColor = parseFloat(this.value);
+    document.querySelector("#background-color-value").innerHTML = backgroundColor;
+}
+
+document.querySelector("#ball-opacity").oninput = function () {
+    ballOpacity = parseFloat(this.value);
+    document.querySelector("#ball-opacity-value").innerHTML = ballOpacity;
+}
+
+document.querySelector("#ball-feathering-steps").oninput = function () {
+    ballFeatheringSteps = parseInt(this.value);
+    document.querySelector("#ball-feathering-steps-value").innerHTML = ballFeatheringSteps;
+}
+
+document.querySelector("#ball-feathering-thick").oninput = function () {
+    ballFeatheringThickness = parseInt(this.value);
+    document.querySelector("#ball-feathering-thick-value").innerHTML = ballFeatheringThickness;
+}
+
+document.querySelector("#draw-mags").addEventListener('change', e => {
+    shouldDrawMags = e.currentTarget.checked;
+});
+
+document.querySelector("#draw-balls").addEventListener('change', e => {
+    shouldDrawBalls = e.currentTarget.checked;
+});
+
+document.querySelector("#dither").addEventListener('change', e => {
+    shouldDither = e.currentTarget.checked;
+});
+
+document.querySelector("#ball-count").oninput = function () {
+    ballCount = parseInt(this.value);
+    document.querySelector("#ball-count-value").innerHTML = ballCount;
+}
+
+document.querySelector("#canvas-width").oninput = function () {
+    canvasWidth = parseInt(this.value);
+    document.querySelector("#canvas-width-value").innerHTML = canvasWidth;
+}
+
+document.querySelector("#canvas-height").oninput = function () {
+    canvasHeight = parseInt(this.value);
+    document.querySelector("#canvas-height-value").innerHTML = canvasHeight;
+}
 
 navigator.mediaDevices
     .getUserMedia({ video: false, audio: true })
@@ -116,17 +115,18 @@ navigator.mediaDevices
         const frequencies = new Uint8Array(bufferLength);
         source.connect(analyzer);
         function animate() {
-            // TODO: control the framerate
-            requestAnimationFrame(animate)
+            requestAnimationFrame(animate);
 
-            ctx.fillStyle = '#010101';
+            ctx.fillStyle = `rgb(${backgroundColor}, ${backgroundColor}, ${backgroundColor})`;
             ctx.fillRect(0, 0, canvas.width, canvas.height);
 
             analyzer.getByteFrequencyData(frequencies);
             drawMagnets(frequencies, audioCtx.sampleRate);
             drawFilings();
 
-            dither(canvas, ctx);
+            if (shouldDither) {
+                dither(canvas, ctx);
+            }
         }
 
         animate();
@@ -137,9 +137,10 @@ class Magnet {
         this.x = x;
         this.y = y;
         this.name = name;
-        this.radius = 30;
 
         this.magnetism = 0;
+
+        this.octaves = Object.entries(notes).filter(([n, f]) => n.slice(0, -1) === name).map(([n, f]) => f);
     }
 
     draw() {
@@ -151,6 +152,8 @@ class Magnet {
     }
 
     update(m) {
+        this.radius = magRadius;
+
         this.magnetism *= 0.9;
         this.magnetism += m;
         if (this.magnetism > 255) {
@@ -159,7 +162,9 @@ class Magnet {
             this.magnetism = 0;
         }
 
-        this.draw();
+        if (shouldDrawMags) {
+            this.draw();
+        }
     }
 }
 
@@ -170,33 +175,36 @@ class Filing {
         this.xVel = 0;
         this.yVel = 0;
 
-        this.radius = 50;
-
         this.color = 'white';
-        this.opacity = 0.1;
 
         this.charge = 0 + (Math.random());
     }
 
     draw() {
         ctx.beginPath();
-
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
-        ctx.globalAlpha = this.opacity;
+        ctx.globalAlpha = ballOpacity;
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fill();
-
-        // poor man's radial gradient
-        // TODO: more of this, and parameterize it
-        ctx.globalAlpha = this.opacity * .5;
-        ctx.arc(this.x, this.y, this.radius * 1.1, 0, Math.PI * 2, false)
-        ctx.fill();
-
         ctx.closePath();
+
+        // feathering
+        ctx.strokeStyle = 'white';
+        for (let i = 0; i < ballFeatheringSteps; i++) {
+            ctx.beginPath();
+            ctx.lineWidth = ballFeatheringThickness;
+            ctx.globalAlpha = ballOpacity * (1 - (i / ballFeatheringSteps));
+            const rad = this.radius - (ballFeatheringThickness / 2) + ((i + 1) * ballFeatheringThickness);
+            ctx.arc(this.x, this.y, rad, 0, Math.PI * 2, false);
+            ctx.stroke();
+        }
+
         ctx.globalAlpha = 1;
     }
 
     update(xAccel, yAccel) {
+        this.radius = ballRadius;
+
         // static accelerations
         this.yVel += gravity;
         this.yVel = this.yVel * (1 - friction);
@@ -244,23 +252,9 @@ class Filing {
             this.yVel *= bounceFactor;
         }
 
-        this.draw();
-    }
-}
-
-const magnets = [];
-const gridCols = 4;
-const gridRows = 3;
-const xSpacing = canvas.width / gridCols + 1;
-const ySpacing = canvas.height / gridRows + 1;
-
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 4; j++) {
-        magnets.push(new Magnet(
-            (j * xSpacing) + (xSpacing / 2),
-            (i * ySpacing) + (ySpacing / 2),
-            twelveNotes[(i * 4) + j]
-        ));
+        if (shouldDrawBalls) {
+            this.draw();
+        }
     }
 }
 
@@ -268,28 +262,20 @@ const hzToIdx = (length, hz, sampleRate) => Math.floor(length * (hz / (sampleRat
 
 const drawMagnets = (frequencies, sampleRate) => {
     magnets.forEach((m, i) => {
-        // TODO: don't calculate this every frame
-        const octaves = Object.entries(notes).filter(([name, freq]) => name.slice(0, -1) === m.name).map(([name, freq]) => freq);
         let dMag = 0;
-        octaves.forEach(f => {
+        m.octaves.forEach(f => {
             freqIdx = hzToIdx(frequencies.length, parseInt(f), sampleRate);
-            dMag += (frequencies[freqIdx] -50) / octaves.length;
+            dMag += (frequencies[freqIdx] - 50) / m.octaves.length;
         });
         m.update(Math.max(0, dMag));
     });
 };
-
-const filings = [];
-for (let i = 0; i < 100; i++) {
-    filings.push(new Filing(Math.random() * canvas.width, Math.random() * canvas.height));
-}
 
 const drawFilings = () => {
     filings.forEach(filing => {
         let xAccel = 0;
         let yAccel = 0;
 
-        // TODO: negatively magnetize the walls (or maybe just the corners);
         magnets.forEach(magnet => {
             a = magnet.x - filing.x;
             b = magnet.y - filing.y;
@@ -305,3 +291,34 @@ const drawFilings = () => {
         filing.update(xAccel, yAccel);
     });
 }
+
+let magnets;
+let filings;
+
+const init = () => {
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    const gridCols = 4;
+    const gridRows = 3;
+    const xSpacing = canvas.width / gridCols + 1;
+    const ySpacing = canvas.height / gridRows + 1;
+
+    magnets = [];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 4; j++) {
+            magnets.push(new Magnet(
+                (j * xSpacing) + (xSpacing / 2),
+                (i * ySpacing) + (ySpacing / 2),
+                twelveNotes[(i * 4) + j]
+            ));
+        }
+    }
+
+    filings = [];
+    for (let i = 0; i < ballCount; i++) {
+        filings.push(new Filing(Math.random() * canvas.width, Math.random() * canvas.height));
+    }
+}
+
+init();
